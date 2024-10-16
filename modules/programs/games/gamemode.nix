c5a3a17119997c439382bc24ddbddc266f351e7b
Martin Tielemans <tielemans.dk@gmail.com>
2024-10-16 20:44:18 +0200

    Gamemode + open discord

--- modules/programs/games/gamemode.nix
+++ modules/programs/games/gamemode.nix
@@ -1,3 +1,12 @@
-{ ...}: {
+{ pkgs, ... }: {
   programs.gamemode.enable = true;
+  home-manager.sharedModules = [
+    (_: {
+      nixpkgs.config.allowUnfree = true;
+
+      home.packages = with pkgs; [
+        gamemode
+      ];
+    })
+  ];
 }
